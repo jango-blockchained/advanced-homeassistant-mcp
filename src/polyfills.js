5f96287688d39dd1a49cef66f4043ede4448d20b
jango-blockchained <info@cryptolinx.de>
2025-02-08 17:24:37 +0100

    Move documentation to gh-pages branch

--- src/polyfills.js
+++
@@ -1,9 +0,0 @@
-// Add necessary polyfills for Node.js compatibility in Bun
-import { webcrypto } from 'node:crypto';
-
-// Polyfill for crypto.subtle in Bun
-if (!globalThis.crypto?.subtle) {
-    globalThis.crypto = webcrypto;
-}
-
-// Add any other necessary polyfills here 
