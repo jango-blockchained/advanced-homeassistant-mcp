528d52a3ab6855c543720c8c8f2b91f1d6f889be
jango-blockchained <info@cryptolinx.de>
2025-11-06 03:47:33 +0100

    Remove test environment files for cleaner project structure

--- test-env.js
+++
@@ -1,4 +0,0 @@
-import { config } from "dotenv";
-config();
-console.log("Token from env:", process.env.HASS_TOKEN);
-console.log("Token length:", process.env.HASS_TOKEN?.length || 0);
--- test-env2.js
+++
@@ -1,3 +0,0 @@
-import { config } from "dotenv";
-config({ path: ".env.development" });
-console.log("Token from .env.development:", process.env.HASS_TOKEN);
